e81762dff197f8a6585adcf2371326352a351adf
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-04-09 16:15:19 +0100

    add park class declaration

--- park.js
+++ park.js
@@ -1,8 +1,52 @@
-// park.name property (pass as arg)
-// park.ticketPrice property (pass as arg)
-// park.dinos = [];
-// park.addDino(dino)
-// park.removeDino() - pops one from array?
-// park.findTopAttraction(dinos) - returns dino with highest visitor appeal
-// park.returnSpecies(species, dinos) - returns an array of all dinos of a particular species
+class Park {
+  constructor(name, ticketPrice) {
+    this._name = name;
+    this._ticketPrice = ticketPrice;
+    this.dinos = [];
+  };
+  get _name() {
+    return this._name;
+  };
+  get ticketPrice() {
+    return this._ticketPrice;
+  };
+  addDino(dino) {
+    this.dinos.push(dino);
+  };
+  removeDino() {
+    this.dinos.pop();
+  };
+  findTopAttraction(dinos) {
+    let top = dinos[0].guestsAttractedPerDay();
+    let topDino = dinos[0];
+    for (let dino of dinos) {
+      if (dino.guestsAttractedPerDay() > top) {
+        top = dino.guestsAttractedPerDay();
+        topDino = dino;
+      };
+    };
+    return topDino.name;
+  };
+  returnSpecies(species, dino) {
+    let speciesDinos = [];
+    for (let dino of dinos) {
+      if (dino.species === species) {
+        speciesDinos.push(dino);
+      };
+    };
+    return speciesDinos;
+  };
+
+  removeSpecies(species, dinos) {
+    let filteredDinos = [];
+    for (let dino of dinos) {
+      if (dino.species !== species) {
+        filteredDinos.push(dino);
+      };
+    };
+    return filteredDinos;
+  };
+
+};
+
 // park.removeSpecies(species, dinos) - removes all dinos of a particular species from the array.
